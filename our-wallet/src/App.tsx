be839d992c73f1923b28c82569eae26f8807dd25
leon123858 <a0970785699@gmail.com>
2023-12-06 00:40:33 +0800

    add PWA

--- our-wallet/src/App.tsx
+++ our-wallet/src/App.tsx
@@ -7,8 +7,21 @@ import Dashboard from "./components/Dashboard.tsx";
 import {Avatar, Col, message, Row} from "antd";
 import Coins from "./components/Coins.tsx";
 import Record from "./components/Record.tsx";
+import { registerSW } from 'virtual:pwa-register';
+
 
 function App() {
+    const updateSW = registerSW({
+        onNeedRefresh() {
+            const confirmed = window.confirm(
+                "A new version is available! Reload to update?"
+            );
+            if (confirmed) {
+                updateSW(true).then(r => console.log(r));
+            }
+        },
+        onOfflineReady() {},
+    });
     return (
         <UserContextProvider>
             <AppHeader menu={<AppMenu getUserInfo={getCurrentUser}/>}/>
@@ -18,7 +31,7 @@ function App() {
                     <Dashboard
                         avatarSrc={<Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" />}
                         name="OurCoin"
-                        description="Your local cryptocurrency"
+                        description="local cryptocurrency"
                         dollars={<h2>1000(coins)</h2>}
                         onButtonClick={async () => {
                             message.info('Followed');
--- our-wallet/vite.config.ts
+++ our-wallet/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {VitePWA} from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), VitePWA({
+        manifest: {
+            "name": 'OurChain Wallet',
+            "description": "A Wallet demo built with OurChain",
+            "theme_color": "#242424",
+
+            // 为了方便，使用svg图标
+            icons: [
+                {
+                    "src": "/vite.svg",
+                    "sizes": "192x192",
+                    "type": "image/svg+xml"
+                },
+                {
+                    "src": "/vite.svg",
+                    "sizes": "512x512",
+                    "type": "image/svg+xml"
+                }
+            ]
+        },
+        devOptions: {
+            // 如果想在`vite dev`命令下调试PWA, 必须启用它
+            enabled: true,
+            // Vite在dev模式下会使用浏览器原生的ESModule，将type设置为`"module"`与原先的保持一致
+            type: "module"
+        }
+    })],
 })
