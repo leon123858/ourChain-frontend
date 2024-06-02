1a6bfa6dd2ad107c933f012a97c6796e4d0bf6e3
leon123858 <a0970785699@gmail.com>
2024-06-03 04:52:58 +0800

    update

--- ourchain-web-cli/src/components/InputPanel.tsx
+++ ourchain-web-cli/src/components/InputPanel.tsx
@@ -1,23 +1,41 @@
-import {Flex} from "antd"
-import React from "react";
-import InputPrivateKey from "./InputPrivateKey.tsx";
-import InputTxBox from "./InputTxBox.tsx";
-import InputPureContract from "./InputPureContract.tsx";
+import { Flex } from 'antd';
+import React, { useEffect } from 'react';
+import InputPrivateKey from './InputPrivateKey.tsx';
+import InputTxBox from './InputTxBox.tsx';
+import InputPureContract from './InputPureContract.tsx';
 
 function InputPanel() {
-    const [address, setAddress] = React.useState("")
-    const [privateKey, setPrivateKey] = React.useState("")
-    const [fixedKey, setFixedKey] = React.useState(false)
+	const [address, setAddress] = React.useState('');
+	const [privateKey, setPrivateKey] = React.useState('');
+	const [fixedKey, setFixedKey] = React.useState(false);
 
-    return <Flex vertical gap={16}>
-        <InputPrivateKey address={address} setAddress={setAddress} privateKey={privateKey} setPrivateKey={setPrivateKey}
-                         fixedKey={fixedKey}
-                         setFixedKey={setFixedKey}/>
-        <InputPureContract/>
-        {
-            fixedKey && privateKey.length > 0 ? <InputTxBox ownerAddress={address} privateKey={privateKey}/> :
-                <h3>請輸入私鑰</h3>
-        }</Flex>
+	useEffect(() => {
+		const savedPrivateKey = localStorage.getItem('privatekey');
+		const savedAddress = localStorage.getItem('address');
+		if (savedPrivateKey && savedAddress) {
+			setPrivateKey(savedPrivateKey);
+			setAddress(savedAddress);
+		}
+	}, []);
+
+	return (
+		<Flex vertical gap={16}>
+			<InputPrivateKey
+				address={address}
+				setAddress={setAddress}
+				privateKey={privateKey}
+				setPrivateKey={setPrivateKey}
+				fixedKey={fixedKey}
+				setFixedKey={setFixedKey}
+			/>
+			<InputPureContract />
+			{fixedKey && privateKey.length > 0 ? (
+				<InputTxBox ownerAddress={address} privateKey={privateKey} />
+			) : (
+				<h3>請輸入私鑰</h3>
+			)}
+		</Flex>
+	);
 }
 
-export default InputPanel
+export default InputPanel;
--- ourchain-web-cli/src/components/InputPrivateKey.tsx
+++ ourchain-web-cli/src/components/InputPrivateKey.tsx
@@ -1,39 +1,55 @@
-import {Input, Switch, Typography} from "antd";
-import React from "react";
+import { Input, Switch, Typography } from 'antd';
+import React from 'react';
 
-function InputPrivateKey({address, setAddress, privateKey, setPrivateKey, fixedKey, setFixedKey,}:
-                             {
-                                 address: string,
-                                 setAddress: React.Dispatch<React.SetStateAction<string>>,
-                                 privateKey: string,
-                                 setPrivateKey: React.Dispatch<React.SetStateAction<string>>,
-                                 fixedKey: boolean
-                                 setFixedKey: React.Dispatch<React.SetStateAction<boolean>>
-                             }) {
-    return <>
-        <div>
-            <Typography.Title level={5}>Private Key (WIF)</Typography.Title>
-            <Switch checkedChildren="鎖定" unCheckedChildren="開啟" checked={fixedKey} onClick={() => {
-                setFixedKey(!fixedKey)
-            }}/>
-            <Input
-                value={address}
-                disabled={fixedKey}
-                placeholder={"address"}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setAddress(e.target.value)
-                }}
-            />
-            <Input
-                value={privateKey}
-                disabled={fixedKey}
-                placeholder={"private key"}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setPrivateKey(e.target.value)
-                }}
-            />
-        </div>
-    </>
+function InputPrivateKey({
+	address,
+	setAddress,
+	privateKey,
+	setPrivateKey,
+	fixedKey,
+	setFixedKey,
+}: {
+	address: string;
+	setAddress: React.Dispatch<React.SetStateAction<string>>;
+	privateKey: string;
+	setPrivateKey: React.Dispatch<React.SetStateAction<string>>;
+	fixedKey: boolean;
+	setFixedKey: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+	return (
+		<>
+			<div>
+				<Typography.Title level={5}>Private Key (WIF)</Typography.Title>
+				<Switch
+					checkedChildren='鎖定'
+					unCheckedChildren='開啟'
+					checked={fixedKey}
+					onClick={() => {
+						setFixedKey(!fixedKey);
+						// save to local storage
+						localStorage.setItem('privatekey', privateKey);
+						localStorage.setItem('address', address);
+					}}
+				/>
+				<Input
+					value={address}
+					disabled={fixedKey}
+					placeholder={'address'}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						setAddress(e.target.value);
+					}}
+				/>
+				<Input
+					value={privateKey}
+					disabled={fixedKey}
+					placeholder={'private key'}
+					onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+						setPrivateKey(e.target.value);
+					}}
+				/>
+			</div>
+		</>
+	);
 }
 
-export default InputPrivateKey
+export default InputPrivateKey;
